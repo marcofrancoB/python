1334573ccfa56926fc2c62fd5bcca44a37260a94
Marco Augusto <105178920+marcofrancoB@users.noreply.github.com>
2024-03-12 21:09:56 -0300

    Add files via upload
    Exemplo de input com nome e idade em py

---
+++ input.py
@@ -0,0 +1,10 @@
+nome=input("Digite seu nome:      ")
+idade=input("Digite sua idade     ")
+print("Seu nome é {} e você tem {} anos".format(nome, idade))
+
+#exemplo02
+
+nome=input("Digite seu nome:      ")
+idade=input("Digite sua idade     ")
+print(f"Seu nome é {nome} e tem {idade} anos")
+
